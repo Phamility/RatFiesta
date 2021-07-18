664b6a10a0f5f0a25367c223080007fea0ba9997
Phamility <32528056+Phamility@users.noreply.github.com>
2021-07-17 22:50:14 -0700

    Update DestroyerBuff.cs

--- Codes/Buffs/DestroyerBuff.cs
+++ Codes/Buffs/DestroyerBuff.cs
@@ -1,4 +1,3 @@
-﻿
 using Microsoft.Xna.Framework;
 using RatFiesta.Buffs;
 using RatFiesta.Minions;
@@ -18,25 +17,21 @@ namespace RatFiesta.Buffs
 {
 	class DestroyerBuff : ModBuff
 	{
-
 		public override void SetDefaults()
 		{
-
-
 			DisplayName.SetDefault("Destroyer");
 			Description.SetDefault("After defeating it's father and siblings, it joins you!");
 			Main.buffNoSave[Type] = true;
 			canBeCleared = false;
-
 			Main.buffNoTimeDisplay[Type] = true;
-
-
 		}
 		public override void Update(Player player, ref int buffIndex)
 		{
 			int expertdamage;
 			if (Main.expertMode == true) { expertdamage = 80; } else { expertdamage = 50; }
+			//I set the probe damage to be 50 in normal worlds and 80 in expert modes, as expert mobs are healthier and tankier
 			if (player.ownedProjectileCounts[ModContent.ProjectileType<ProbeSummon>()] <= 0)
+			//IF there is not a single probe out there, I summon one and give the buff.
 			{
 				Projectile.NewProjectile(player.position.X, player.position.Y, 0f, 0f, ModContent.ProjectileType<ProbeSummon>(), expertdamage, -5f, Main.myPlayer);
 
@@ -44,17 +39,16 @@ namespace RatFiesta.Buffs
 			}
 			else
 			{
-
 				player.DelBuff(buffIndex);
 				buffIndex--;
+				//To be completely honest, I have no clue what this part is for, but I have a feeling that it's important, screwing  something in this
+					//part up either non stops summons projectiles or none at all, so I would add it just in case lol...
 
 			}
 		}
 	}
 }
-	
-		//	
-	
+
 
 
 	
