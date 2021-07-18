18df68df592fa7f1a4083af0d4db4bcdc6ea70a8
Phamility <32528056+Phamility@users.noreply.github.com>
2021-07-17 21:41:48 -0700

    Update RockFloat.cs

--- Codes/Accessories/RockFloat.cs
+++ Codes/Accessories/RockFloat.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using RatFiesta.Buffs;
 using RatFiesta.Minions;
 using System;
@@ -15,75 +15,45 @@ namespace RatFiesta.Items.Accessories
 {
     class RockFloat : ModItem
     {
-
-
-
-
         public override void SetDefaults()
         {
-
             item.width = 32;
             item.height = 32;
             item.accessory = true;
             item.value = Item.sellPrice(platinum: 1);
             item.rare = ItemRarityID.Orange;
-            // item.summon = true;
-            item.UseSound = SoundID.Item113;
             item.buffType = ModContent.BuffType<RockBuff>();
             item.noMelee = true;
             item.noUseGraphic = true;
-
+            //These two last lines aren't necessary, I just added them when copying and pasting some defaults.
         }
-
-
         public override void SetStaticDefaults()
         {
-        
-
             DisplayName.SetDefault("Mineral Manipulator");
-
             Tooltip.SetDefault("Wearing this item summons 75 Rocks that fly around you and do 4 damage.\n" +
                 "Damage is increased to 5 in expert mode.\nDamage isn't affected by buffs.\n" +
                 "Deeeeefiniely not a good in single-target scenarios.");
-
         }
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
             recipe.AddIngredient(ItemID.StoneBlock, 75);
             recipe.AddRecipeGroup(mod.Name + ":Evil", 25);
+            //Reference RatFiesta -> Codes -> Other
             recipe.AddIngredient(ItemID.GravitationPotion, 1);
-
             recipe.SetResult(this);
             recipe.AddRecipe();
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-
-
-
-
-
             bool HasBuff = player.HasBuff(ModContent.BuffType<RockBuff>());
-
-
-
             if (HasBuff != true)
-
-            // if (player.ownedProjectileCounts[ModContent.ProjectileType<ProbeSummon>()] <= 0)
             {
-                //   Projectile.NewProjectile(player.position.X, player.position.Y, 0f, 0f, ModContent.ProjectileType<ProbeSummon>(), 50, -5f, Main.myPlayer);
-
                 player.AddBuff(item.buffType, 2);
             }
-
-
-
-
+//Thinking back, I think you can remove a lot and just have "player.AddBuff(item.buffType, 2);
+// The 2 means 2/60 or 1/30 of a second by the way.
         }
-
     }
-
 }
-
