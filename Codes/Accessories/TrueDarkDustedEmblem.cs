4ab38dedbb2cf0769215615523246dd439b74706
Phamility <32528056+Phamility@users.noreply.github.com>
2021-07-17 21:57:49 -0700

    Update TrueDarkDustedEmblem.cs

--- Codes/Accessories/TrueDarkDustedEmblem.cs
+++ Codes/Accessories/TrueDarkDustedEmblem.cs
@@ -1,8 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using RatFiesta.Buffs;
 using RatFiesta.Minions;
 using RatFiesta.Items.XConsumables;
-
 using Terraria;
 using System.IO;
 using Terraria.ID;
@@ -25,7 +24,7 @@ namespace RatFiesta.Items.Accessories.EmblemSetUp
             item.value = Item.sellPrice(platinum: 20);
             item.rare = ItemRarityID.Expert;
             item.expert = true;
-
+            //The second to last line makes it so that the item appears a rainbow color, the last line makes it so it doesn't change color when reforged.
         }
 
         public override void SetStaticDefaults()
@@ -38,31 +37,30 @@ namespace RatFiesta.Items.Accessories.EmblemSetUp
         public override void UpdateAccessory(Player player, bool showVisual)
         {
             if (showVisual == true)
+            //This means that if the icon right of the accessory is greyd out, or NOT visible, this results true and you lose 4 minion slots, though you gain 180% minions damage
             {
                 player.minionDamage += 1.8f;
                 player.maxMinions -= 4;
                 
             }
             else
+            //If it IS visible, no minion slots are loss and +90% damage is gained.
+            //Unrelated to code, but wish more sacrifical concepts like these exist, though the problem here is you can some Max minions - 4 with boosted damage, then make it visible
+                //and put out 4 slightly boosted minions, but whateeeeeever.
             {
                 player.minionDamage += .9f;
             }
         }
-        //ON OFF FOR MEGA SPIKE DAMAGE -6 SLOTS
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
             recipe.AddIngredient(ModContent.ItemType<TrueSundustedEmblem>());
             recipe.AddIngredient(ModContent.ItemType<NDarkContinentFragment>());
-           
+         //Previous 2 lines reference modded content, if you wanted vanilla items as ingredients, use "(ItemID."ThatItemsName)", ofc without the quotes. 
             recipe.AddTile(TileID.LunarCraftingStation);
+            //Makes it so it requires the Ancient Manipulator, yes it's called Lunar Crafting Station in code.
             recipe.SetResult(this);
             recipe.AddRecipe();
-           
         }
-
-
     }
-
 }
-
