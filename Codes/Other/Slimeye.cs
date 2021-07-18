4459afa745cbfec726f3d9934eea2314527d6d12
Phamility <32528056+Phamility@users.noreply.github.com>
2021-07-18 00:40:24 -0700

    Update Slimeye.cs

--- Codes/Other/Slimeye.cs
+++ Codes/Other/Slimeye.cs
@@ -1,4 +1,4 @@
-﻿using RatFiesta.Buffs;
+using RatFiesta.Buffs;
 using RatFiesta.Minions;
 using RatFiesta.Items.XConsumables;
 
@@ -24,44 +24,53 @@ namespace RatFiesta
         {
             DisplayName.SetDefault("Slimeye");
             Main.npcFrameCount[npc.type] = 2;
-
+//Two frames of the demon eye ai
         }
         public override void SetDefaults()
         {
-
+        //THE MOST BOOTLEG BOSS YOU COULD EVER GET
             npc.boss = true;
             npc.width = 152;
             npc.height = 110;
+            //I got eye of cthlulu sizes 
             npc.npcSlots = 5;
             npc.aiStyle = 2;
+            //This is the aistyle of the demon eye, 
             npc.noTileCollide = true;
+            //This is smt I changed abt the ai type so it didnt look weird
             npc.lavaImmune = true;
             music = MusicID.Boss5;
+            //Muuuuuusic
             bossBag = mod.ItemType("BossBag");
-            //npc.lifeRegen
+         //I called my item BossBag btw, so U put urs.
             animationType = NPCID.DemonEye;
+            //Simple two framer
 
 
             npc.lifeMax = 700;
             npc.damage = 24;
             npc.defense = 4;
+            //some statsss
 
             npc.HitSound = SoundID.NPCHit1;
             npc.DeathSound = SoundID.NPCDeath1;
+            // simple sounds
 
 
             npc.knockBackResist = .08f;
+            //recommended 0, i did this cuz it's a pre king slime boss aka easy af
 
-            //npc.buffImmune[20] = true;
+         
             aiType = NPCID.DemonEye;
-
+            //ai type that of the demon eye
         }
         public override void ScaleExpertStats(int numPlayers, float bossLifeScale)
         {
             npc.lifeMax = (int)(npc.lifeMax * (bossLifeScale ));
             npc.damage = (int)(npc.damage * 1.3f);
+            //this is necessary to scale your boss in expert / multiplayer
         }
-        // public override float SpawnChance(NPCSpawnInfo spawninfo) { return SpawnCondition.OverworldDay.Chance * .12f; }
+
         private bool isit = true;
         public override void AI()
         {
@@ -76,18 +85,22 @@ namespace RatFiesta
                 {
                     npc.timeLeft = 20;
                     return;
-
-                }           }
+//THis ordeal makes it go away when the player dies
+                }         
+                }
 
          
             if (Main.expertMode)
+            //These are some experted changes
             {
                 if (npc.life <= 500)
                 {
+                //Quote on quote second phase lol, i did jack with coding, I just raised it's defense
                     npc.defense = 6;
                 }
                 if (npc.life <= 9)
                 {
+                //Makes it so you have to do 10+ damage to kill it, this is my unique trait - prob shouldnt do this, idek what im looking at.
                     if (isit == true)
                     {
                         npc.life += 1;
@@ -104,12 +117,15 @@ namespace RatFiesta
         public override void NPCLoot()
         {
             ZaWarudo.DownedSlimeye = true;
+            //Reference Rat Fiesta -> Codes -> Other
             if (Main.expertMode == true)
             {
                 npc.DropBossBags();
+                //drops bag in expert
             }
             else
             {
+            //splurs objects in non expert
                 Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.GoldCoin, Main.rand.Next(2, 4), true, -1);
              
 
@@ -117,6 +133,7 @@ namespace RatFiesta
 
                 if (Main.rand.Next(1, 5) != 2)
                 {
+                //75% chance to drop one of the two loot
                     if (Main.rand.Next(1, 3) == 2)
                     {
                         Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("GooeyeBear"), 1, true, -1);
@@ -132,6 +149,7 @@ namespace RatFiesta
         }
         public override void BossLoot(ref string name, ref int potionType)
         {
+        //Made it so in expert, potions dont splur out, and in normal, some Bengays drop out (bengays is a potion btw)
             if (Main.expertMode == true) { potionType = 0; }
             if (Main.expertMode == false) { potionType = ModContent.ItemType<ABenGay>(); }
 
