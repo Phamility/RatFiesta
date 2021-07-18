b4679397287afb7d59fa8a419889192b98ed843c
Phamility <32528056+Phamility@users.noreply.github.com>
2021-07-17 21:50:32 -0700

    Update SigilOfTheWorld.cs

--- Codes/Accessories/SigilOfTheWorld.cs
+++ Codes/Accessories/SigilOfTheWorld.cs
@@ -24,21 +24,23 @@ namespace RatFiesta.Items.Accessories.Sigils
         public override void SetDefaults()
         {
             Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(30, 2));
+            //The 30 means it switches frame (60/30) a second, the 2 means how many frames their are on the sprite. 
+            // When making animation sprites, make sure they are vertical and a padding of 2.
             item.width = 52;
             item.height = 52;
             item.accessory = true;
             item.value = Item.sellPrice(platinum: 10);
             item.rare = ItemRarityID.Expert;
-            // item.summon = true;
             ItemID.Sets.ItemNoGravity[item.type] = true;
+            //Makes the item float
             item.buffType = ModContent.BuffType<DestroyerBuff>();
-            item.noMelee = true;
-            item.noUseGraphic = true;
+            //Reference Rat Fiesta -> Codes -> Buffs
             item.expert = true;
         }
         public override void SetStaticDefaults()
         {
             ItemID.Sets.ItemNoGravity[item.type] = true;
+            //Looking back, I wrote this line twice .-. One of them work :D
 
             DisplayName.SetDefault("Sigil Of The World");
             Tooltip.SetDefault("Increases Damage by 15%. Increases Movement Speed by 20% \nIncreases max amount of minions by 6. Largely increases Minion Knockback.  " +
@@ -60,16 +62,19 @@ namespace RatFiesta.Items.Accessories.Sigils
             recipe.AddTile(TileID.LunarCraftingStation);
             recipe.SetResult(this);
             recipe.AddRecipe();
+            //Boot leg Fargos :D
         }
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             player.runAcceleration *= 1.2f;
             player.maxRunSpeed *= 1.2f;
-            
+            //The +20% Movement Speed
             player.maxMinions += 6;
+            //When equipped +6 Max minions.
             player.minionKB += 3;
+            //For reference, hercules beetle adds +1 knockback.
             player.allDamage += .15f;        
-            
+            //The +15% Damage
             player.AddBuff(BuffID.WellFed, 2);
             player.AddBuff(BuffID.Spelunker, 2);
             player.AddBuff(BuffID.Endurance, 2);
@@ -82,29 +87,36 @@ namespace RatFiesta.Items.Accessories.Sigils
             player.AddBuff(BuffID.ObsidianSkin, 2);
             player.AddBuff(BuffID.Lifeforce, 2);
             player.AddBuff(BuffID.Wrath, 2);
+            //Adds a bunch of buffs for 2/60 a second when equipped (When taking off, they'll go away duh)
 
             player.statManaMax2 += 100;
             player.statLifeMax2 += 100;
+            //NEVER TOUCH STATLIFEMAX OR STATMANAMAX, NEVER!!!
+            //statLifeMax2 and statManaMax2 adds stats to the player's health and mana. 
 
             player.wingTime *= 100;
-            
+            //Tbh, I wanted to make a 15% wing flight time, but didn't know how (First mod) , so I just did this to make them fly forever :D
             player.statDefense += 15;
+            //Simply adds 15 defense.
             
             bool HasBuff = player.HasBuff(ModContent.BuffType<DestroyerBuff>());
-   
-
             if (hideVisual == true)
+            //That icon right of your accessories, if it's greyed out, that means hideVisual == true, that means that in this case, the lighting around the player is reduced
             {
                 Lighting.AddLight(player.position, 1.0f, 1.0f, 1.0f);
             }
             else
+            //Else, that means if it's visibile, in this case, their will be a bright light surrounding the player.
+            
             {
                 if (HasBuff != true)
              {
                     player.AddBuff(item.buffType, 2);
+                    //This "Destroyer Buff" is only added when the accessory is visible.
              }
                 Lighting.AddLight(player.position, 14f, 14f, 14f);
                 player.AddBuff(ModContent.BuffType<TrueSightBuff>(), 2);
+                //Reference Rat Fiesta -> Codes -> Buffs
             }
         }
     }
